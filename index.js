d0eeadc52acb17d3cafd6b2309fb254b14c1d343
Poornimabatham <pbatham21@gmail.com>
2025-01-22 15:59:23 +0530

    add code

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-function sumArray(arr) {
+function list(...args) {
+    return args;
+  }
+  
+  function addArguments(arg1, arg2) {
+    return arg1 + arg2;
+  }
+  
+//   console.log(list(1, 2, 3)); // [1, 2, 3]
+  
+//   console.log(addArguments(1, 2)); // 3
+  
+  // Create a function with a preset leading argument
+  const leadingThirtySevenList = list.bind(null, 37,98);
+  
+  // Create a function with a preset first argument.
+  const addThirtySeven = addArguments.bind(null, 37);
+  
+  console.log(leadingThirtySevenList()); // [37]
+  console.log(leadingThirtySevenList(1, 2, 3)); // [37, 1, 2, 3]
+  console.log(addThirtySeven(5)); // 42
+  console.log(addThirtySeven(5, 10)); // 42
+  // (the last argument 10 is ignored)
+  
+
+
+
+
+  function sumArray(arr) {
     let total = 0; // Variable captured by closure
     function helper(index) {
       if (index >= arr.length) {
